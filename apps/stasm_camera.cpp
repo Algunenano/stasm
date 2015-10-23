81e27c49db8d181e4b2970341911b49e95620437
Algunenano <algunenano@gmail.com>
2015-10-23 13:14:07 +0200

    File creation for the camera

---
+++ apps/stasm_camera.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   stasm_camera.cpp
+ * Author: algunenano
+ *
+ * Created on 23 de octubre de 2015, 13:10
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
